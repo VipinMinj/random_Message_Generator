85c6b0bfc9b4babad354362365a1aeb5d328e869
Vipin Minj <166230467+VipinMinj@users.noreply.github.com>
2024-04-14 15:51:26 +0530

    Add funtion message that prints the fortune

--- fortuneTeller.js
+++ fortuneTeller.js
@@ -6,10 +6,10 @@ function randomNumberGenerator(num) {
 
 function fortuneQuotes(fortuneType) {
     
-    //Object that stores all the fortunes
+    //Object that stores all the fortune quotes
     const fortune = {
-        todayPrediction : ['A good day', 'Incredible', 'Strange', 'Scary', 'Quiet', 'Peaceful', 'Awful'],
-        encounter : ['A Cute Animal', 'An Old Friend', 'Your Coworker', 'The Love of your Life', 'Your Nemesis', 'Your Rival'],
+        todayPrediction : ['a good day', 'Incredible', 'Strange', 'Scary', 'Quiet', 'Peaceful', 'Awful'],
+        encounter : ['a Cute Animal', 'An Old Friend', 'Your Coworker', 'The Love of your Life', 'Your Nemesis', 'Your Rival'],
         action : ['Walk Away', 'Face your Fears', 'Confess', `Say what's on your Mind`, `Exercise`],
         wordOfTheDay : [`Exhilarating`,`Exciting`, `Boring`, `Messy`, `Mysterious` ]
     }
@@ -32,3 +32,13 @@ function fortuneQuotes(fortuneType) {
             return `Invalid Input. Please try again.`;
     }
 }
+
+function message() {
+    //This funtion prints the whole message
+    console.log(`Today's gonna be : ${fortuneQuotes('todayPrediction')}`);
+    console.log(`You're gonna meet : ${fortuneQuotes('encounter')}`);
+    console.log(`You should : ${fortuneQuotes('action')}`);
+    console.log(`My word of the day for you is : ${fortuneQuotes('wordOfTheDay')}`);
+}
+
+message();
